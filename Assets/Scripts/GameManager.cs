ac78e8dc6bc003a3df5e09acee1b2d18a11091fe
PaulElcampeon <pauloladele@live.co.uk>
2020-06-06 12:09:35 +0100

    Added skeleton code for GameManager persisting and loading data functionality.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private float timeAlreadyPlayed;
+
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        LoadGameData();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void SaveGameData()
+    {
+        SaveTimeGamePlayed();
+    }
+
+    private void LoadGameData()
+    {
+        timeAlreadyPlayed = PlayerPrefs.GetFloat("time_played");
+
+        Debug.Log("Loading Game Data");
+    }
+
+    public void SaveTimeGamePlayed()
+    {
+        float timePlayedThisSession = Timer.instance.GetTimePlayedThisSession();
+
+        if (PlayerPrefs.HasKey("time_played"))
+        {
+            float combinedTimePlayed = timePlayedThisSession + timeAlreadyPlayed;
+
+            PlayerPrefs.SetFloat("time_played", combinedTimePlayed);
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("time_played", timePlayedThisSession);
+        }
+
+        Debug.Log("Saving Time Played");
+    }
+
+    private void StartGamePlayTimer()
+    {
+        Timer.instance.StartSessionTimer();
+
+        Debug.Log("Starting timer");
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quiting Game...");
+
+        Application.Quit();
+    }
+}
